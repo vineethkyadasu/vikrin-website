ae330bf26a0696fa0317a9da496598da0afd778d
VineethK <kyadasuvineeth@gmail.com>
2025-07-25 02:18:37 +0530

    Add: sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.vikrin.com', // always use www if that's your canonical domain
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+};
